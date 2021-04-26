9bfb08bb2edbc3dd4d90c2670e3163c269d96001
Giocatory <nivolyas@gmail.com>
2021-04-26 10:29:49 +0300

    MyJavaLearn

---
+++ src/ru/giocatory/Learn/OOP/RecursiveFunction/Factorial.java
@@ -0,0 +1,20 @@
+package ru.giocatory.Learn.OOP.RecursiveFunction;
+
+public class Factorial {
+
+    static int calcRecursive(int number){
+        int result;
+
+        if (number == 1 || number == 0) return 1;
+        result = calcRecursive(number - 1) * number;
+        return result;
+    }
+    static int simpleMethod(int number){
+        int multiply = 1;
+        if (number == 0 || number == 1) return 1;
+        for (int i = 1; i <= number; i++){
+            multiply *= i;
+        }
+        return multiply;
+    }
+}
---
+++ src/ru/giocatory/Learn/OOP/RecursiveFunction/factorialCalculation.java
@@ -0,0 +1,21 @@
+package ru.giocatory.Learn.OOP.RecursiveFunction;
+
+public class factorialCalculation {
+    public static void main(String[] args) {
+        // Calculating the factorial using the recursive method
+        System.out.println("Факториал 0 равен " + Factorial.calcRecursive(0));
+        System.out.println("Факториал 1 равен " + Factorial.calcRecursive(1));
+        System.out.println("Факториал 3 равен " + Factorial.calcRecursive(3));
+        System.out.println("Факториал 4 равен " + Factorial.calcRecursive(4));
+        System.out.println("Факториал 5 равен " + Factorial.calcRecursive(5));
+        System.out.println();
+
+        // Calculating the factorial by a simple method
+        System.out.println("Факториал 0 равен " + Factorial.simpleMethod(0));
+        System.out.println("Факториал 1 равен " + Factorial.simpleMethod(1));
+        System.out.println("Факториал 3 равен " + Factorial.simpleMethod(3));
+        System.out.println("Факториал 4 равен " + Factorial.simpleMethod(4));
+        System.out.println("Факториал 5 равен " + Factorial.simpleMethod(5));
+        System.out.println();
+    }
+}
