b23ee8ef214565cf696d353f1df1d79d0537b4d0
Giocatory <nivolyas@gmail.com>
2021-04-30 15:28:44 +0300

    MyJavaLearn

---
+++ src/ru/giocatory/Learn/OOP/AccessControl/Stack.java
@@ -0,0 +1,30 @@
+package ru.giocatory.Learn.OOP.AccessControl;
+
+public class Stack {
+    private final int[] stack = new int[10];
+    private int tos;
+
+    // Initializing the top of the stack
+    Stack(){
+        tos = -1;
+    }
+    // Place an item on the stack
+    void push(int item){
+        if (tos == 9){
+            System.out.println("Stack full");
+        }
+        else {
+            stack[++tos] = item;
+        }
+    }
+    // Pop an item from the stack
+    int pop(){
+        if (tos < 0){
+            System.out.println("Stack not loaded");
+            return 0;
+        }
+        else {
+            return stack[tos--];
+        }
+    }
+}
---
+++ src/ru/giocatory/Learn/OOP/AccessControl/TestStack.java
@@ -0,0 +1,25 @@
+package ru.giocatory.Learn.OOP.AccessControl;
+
+public class TestStack {
+    public static void main(String[] args) {
+        Stack myStackOne = new Stack();
+        Stack myStackTwo = new Stack();
+        // Place numbers in stack
+        for (int i = 0; i < 10; i++){
+            myStackOne.push(i);
+        }
+        for (int i = 10; i < 20; i++){
+            myStackTwo.push(i);
+        }
+        // extract this numbers from stack
+        System.out.println("Stack in \"myStackOne\":");
+        for (int i = 0; i < 10; i++){
+            System.out.println(myStackOne.pop());
+        }
+
+        System.out.println("Stack in \"myStackTwo\":");
+        for (int i = 0; i < 10; i++){
+            System.out.println(myStackTwo.pop());
+        }
+    }
+}
